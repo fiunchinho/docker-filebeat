2c5a80d36fc128ed1f6c9b209cd7d3225ae6696c
José Armesto <github@armesto.net>
2015-11-04 10:22:59 +0100

    Simpler way of compiling from source

--- build_binaries.sh
+++ build_binaries.sh
@@ -4,9 +4,9 @@ echo "Building filebeat..."
 
 git clone https://github.com/elastic/filebeat.git
 cd filebeat
-docker run --rm -w="/go/src/github.com/elastic/filebeat" -v "$PWD:/go/src/github.com/elastic/filebeat" golang make crosscompile
+docker run --rm -w="/go/src/github.com/elastic/filebeat" -v "$PWD:/go/src/github.com/elastic/filebeat" golang make
 
-mv ./bin/filebeat-linux-386 ./../binary/filebeat
+mv ./filebeat ./../binary/filebeat
 cd ..
 rm -rf filebeat
 
