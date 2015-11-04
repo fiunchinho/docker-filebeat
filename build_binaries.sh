49736289d0093c5da0f185c139b6c9265b3c092b
José Armesto <github@armesto.net>
2015-11-04 11:05:56 +0100

    Fixed filebeat binary

--- build_binaries.sh
+++ build_binaries.sh
@@ -6,7 +6,7 @@ git clone https://github.com/elastic/filebeat.git
 cd filebeat
 docker run --rm -w="/go/src/github.com/elastic/filebeat" -v "$PWD:/go/src/github.com/elastic/filebeat" golang make crosscompile
 
-mv ./bin/filebeat-linux-amd64 ./../binary/filebeat
+mv ./bin/filebeat-linux-386 ./../binary/filebeat
 cd ..
 rm -rf filebeat
 
