25d61748e146139b4b652320c95fe9a9b7f2f79f
José Armesto <github@armesto.net>
2015-11-04 10:27:28 +0100

    Fix filebeat compilation

--- build_binaries.sh
+++ build_binaries.sh
@@ -4,9 +4,9 @@ echo "Building filebeat..."
 
 git clone https://github.com/elastic/filebeat.git
 cd filebeat
-docker run --rm -w="/go/src/github.com/elastic/filebeat" -v "$PWD:/go/src/github.com/elastic/filebeat" golang make
+docker run --rm -w="/go/src/github.com/elastic/filebeat" -v "$PWD:/go/src/github.com/elastic/filebeat" golang make crosscompile
 
-mv ./filebeat ./../binary/filebeat
+mv ./bin/filebeat-linux-amd64 ./../binary/filebeat
 cd ..
 rm -rf filebeat
 
