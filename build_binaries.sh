6f829142540404d7e3e412794b3d599e1ef37586
José Armesto <github@armesto.net>
2016-12-12 13:57:48 +0100

    New filebeat version

--- build_binaries.sh
+++ build_binaries.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+VERSION=${VERSION:=v1.3.1}
+
 echo "Building filebeat..."
 
 git clone https://github.com/elastic/beats.git
 cd beats
+git checkout tags/${VERSION}
 docker run --rm -w="/go/src/github.com/elastic/beats/filebeat" -v "$PWD:/go/src/github.com/elastic/beats" -e "GOX_OS=linux" golang make crosscompile
 
 mv ./filebeat/build/bin/filebeat-linux-386 ./../binary/filebeat
